b8b9af4e514d6f5d6089b00864875b200c06f3ad
IvanGriko <grikoivan@yandex.ru>
2024-10-30 10:45:25 +0300

    Add files via upload

---
+++ src/Epic.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class Epic extends Task {
+    protected ArrayList<Subtask> subtaskList = new ArrayList<>();
+
+    public ArrayList<Subtask> getSubtaskList() {
+        return subtaskList;
+    }
+
+    public void setSubtaskList(ArrayList<Subtask> subtaskList) {
+        this.subtaskList = subtaskList;
+    }
+
+    public Epic(String name, String description, int taskCount) {
+        super(name, description, taskCount);
+    }
+
+    @Override
+    public void setStatus(TaskStatus status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "\n" + "Эпик. Название: " + name + ", Описание: " + description +
+                ", Статус: " + getStatus() + ", " + "id " + id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Epic epic = (Epic) o;
+        return Objects.equals(this.getId(), epic.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), subtaskList);
+    }
+}
